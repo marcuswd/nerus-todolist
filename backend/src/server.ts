236cbdfe299ab9e46bdf61a3633c5772aeb8fd79
Marcus Vinicius <marcus@marcuswd.com>
2025-04-16 10:04:20 -0300

    feat: Implementação do Docker e atualização das configurações do Prisma para funcionar no Docker.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { Request } from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerDoc from "./swagger-output.json";
 import bodyParser from "body-parser";
@@ -9,7 +9,7 @@ dotenv.config();
 import TodosRoutes from "./routes/TodosRoutes";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT : number = Number(process.env.PORT) || 3001;
 
 app.use(bodyParser.json());
 app.use(express.json());
@@ -21,6 +21,6 @@ app.use(cors({
 app.use("/api", TodosRoutes);
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
